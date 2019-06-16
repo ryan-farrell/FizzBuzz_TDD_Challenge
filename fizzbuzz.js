68d172fcfe8d30a9759a6bc5422a3e0d38ac3ea8
ryan-farrell <ryan.farrell44@gmail.com>
2019-06-17 00:04:32 +0100

    Completed fizzBuzz with first attempts of using TDD

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,37 @@
-module.exports = fizzbuzz
+module.exports = fizzbuzz // We are making the function below fizzbuzz() available for export to our TDD (test driven development) file test.
 
-function fizzbuzz (number) {
-  // TODO: make this work
+function fizzbuzz (number) {        // Creating our function and setting 'number' as our parameter.
+  // TODO: make this work   
+  if (number % 3 === 0 && number % 5 === 0) {       
+  return 'fizzbuzz'
+  /* Using modulus we are comparing the functions argument against being divided by 3 and 5 and if 
+  the value of this is zero in both calculations it will return the string 'fizzbuzz'.*/
+} else if (number % 5 === 0) {
+  return 'buzz'
+  /* Using modulus we are comparing the functions argument against being divided by only 5 and if 
+  the value of this is zero it will return the string 'buzz'.*/
+} else if (number % 3 === 0) {
+  return 'fizz'
+  /* Using modulus we are comparing the functions argument against being divided by only 3 and if 
+  the value of this is zero it will return the string 'fizz'.*/
+} else {return number}
+/* Using modulus we are comparing the above statements in this functions first 1 by 1 as is the nature of 
+this 'if function'. If the argument fails all three statements above then the value must have a remainder 
+and therefore be caught in this else catch and return itself as the result.*/
+  
 }
+  
+console.log(fizzbuzz(300));
+
+
+/* Created a simple loop to check our function above works when given an argument and loops through 
+returning the correct outcome based on our main function above. */
+
+
+function loopFizzBuzz (num) {
+for (i = 1; i <= num; i++) {
+    console.log(fizzbuzz(i))
+  }
+}
+
+loopFizzBuzz(300);
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,7 +1,95 @@
 const fizzbuzz = require('./fizzbuzz')
 
+/* Created 15 tests using the 'jest' testing framework as my TDD starting point 
+to complete the well known fizzbuzz tech test.*/
+
+/******* Note to myself that further reading on other test libraries/frameworks is 
+required. Although Node.js has its on built in assertion library others are also available
+such as 'Chai' usually used in conjunction with 'Mocha'. Also 'SuperTest' is used with 'Express'******** */
+
 describe('fizzbuzz', () => {
   it('returns 1 when given 1', () => {
     expect(fizzbuzz(1)).toBe(1)
   })
 })
+describe('fizzbuzz', () => {
+  it('returns 2 when given 2', () => {
+    expect(fizzbuzz(2)).toBe(2)
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns fizz when given 3', () => {
+    expect(fizzbuzz(3)).toBe('fizz')
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns 4 when given 4', () => {
+    expect(fizzbuzz(4)).toBe(4)
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns buzz when given 5', () => {
+    expect(fizzbuzz(5)).toBe('buzz')
+  })
+}
+)
+describe('fizzbuzz', () => {
+  it('returns fizz when given 6', () => {
+    expect(fizzbuzz(6)).toBe('fizz')
+  })
+})
+describe('fizzbuzz', () => {
+  it('returns 7 when given 7', () => {
+    expect(fizzbuzz(7)).toBe(7)
+  })
+})
+describe('fizzbuzz', () => {
+  it('returns 8 when given 8', () => {
+    expect(fizzbuzz(8)).toBe(8)
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns fizz when given 9', () => {
+    expect(fizzbuzz(9)).toBe('fizz')
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns buzz when given 10', () => {
+    expect(fizzbuzz(10)).toBe('buzz')
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns 11 when given 11', () => {
+    expect(fizzbuzz(11)).toBe(11)
+  })
+}
+)
+describe('fizzbuzz', () => {
+  it('returns fizz when given 12', () => {
+    expect(fizzbuzz(12)).toBe('fizz')
+  })
+})
+describe('fizzbuzz', () => {
+  it('returns 13 when given 13', () => {
+    expect(fizzbuzz(13)).toBe(13)
+  })
+}
+)
+describe('fizzbuzz', () => {
+  it('returns 14 when given 14', () => {
+    expect(fizzbuzz(14)).toBe(14)
+  })
+})
+
+describe('fizzbuzz', () => {
+  it('returns fizzbuzz when given 15', () => {
+    expect(fizzbuzz(15)).toBe('fizzbuzz')
+  })
+})
+
